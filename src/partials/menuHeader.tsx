b24531ddf49816f95f5b20656c612f5fbcf646d9
citkane <michael@openpoint.ie>
2022-07-15 19:42:29 +0100

    add menuHeader to side bar

---
+++ src/partials/menuHeader.tsx
@@ -0,0 +1,11 @@
+import { DefaultThemeRenderContext, PageEvent, Reflection, JSX } from "typedoc";
+
+export const menuHeader = (context: DefaultThemeRenderContext) => (props: PageEvent<Reflection>) => {
+	return (
+		<>
+			<img id="logo" src={context.relativeURL("assets/logo.svg") as string} alt={props.project.name}/>
+			<h1>{props.project.name}</h1>
+			<input type="search" id='search' />
+		</>
+	);
+}
