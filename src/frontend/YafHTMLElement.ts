3aaf9cd10c6b074eeaa02833a1a176dcc064bec1
citkane <michael@openpoint.ie>
2023-02-03 17:06:48 +0000

    release to 0.0.1

--- src/frontend/YafHTMLElement.ts
+++ src/frontend/YafHTMLElement.ts
@@ -1,5 +1,13 @@
 import { appendChildren } from './yafElement.js';
 
+/**
+ * A base class extension for all custom HTML WebComponents.
+ *
+ * It provides:
+ *  - The often used `appendChildren` utility as a convenience to all Yaf components.
+ *  - overrides the default `connectedCallback` with the purpose of providing a de-bouncer.\
+ *    For inexplicable reasons, some nested custom WebComponents get multiple connected signals.
+ */
 export default class YafHtmlElement<
 	T = Record<string, never>
 > extends HTMLElement {
@@ -7,6 +15,11 @@ export default class YafHtmlElement<
 	appendChildren = appendChildren(this);
 	private debounceCount = 0;
 
+	/**
+	 * The standard Web Component connect entry.
+	 *
+	 * This debounces or triggers the new `onConnect` trigger used in all ancestor Yaf theme components.
+	 */
 	connectedCallback() {
 		if (this.debounceCount) {
 			/*
--- src/frontend/handlers/index.ts
+++ src/frontend/handlers/index.ts
@@ -1,4 +1,4 @@
-export { default as events, Events } from './events/eventApi.js';
+export { default as events, Events } from './events/Events.js';
 export { default as Router } from './Router.js';
 export { default as appState, AppState } from './AppState.js';
 export { default as ErrorHandlers } from './ErrorHandlers.js';
